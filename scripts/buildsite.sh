2a3df9111a41f27154f9e4a3b3bb0e331a699721
Sofia CETIN <sofia.cetin02@etud.univ-paris8.fr>
2026-01-02 23:47:17 +0100

    Modification de la génération du site avec un script bash

---
+++ scripts/buildsite.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Rebuilding site."
+rm -rf public/ # tout supprimer !
+mkdir public # recréer le dossier
+cp -r assets/* public/ # copier les ressources
+for page in pages/*.html; do # pour chaque page ...
+    echo -n "> building ${page#pages/}... "
+    scripts/buildpage.sh "$page" # ... la construire avec notre script
+    echo "done."
+done
+echo "Finished!"
