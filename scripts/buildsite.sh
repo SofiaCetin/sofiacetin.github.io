fa5d65e3985bbcad35f7a134bdfecd80070d66f6
SofiaCetin <sofia.cetin2006@gmail.com>
2026-07-18 17:50:22 +0200

    Update buildsite.sh

--- scripts/buildsite.sh
+++ scripts/buildsite.sh
@@ -6,7 +6,7 @@ mkdir public # recréer le dossier
 cp -r assets/* public/ # copier les ressources
 for page in pages/*.html; do # pour chaque page ...
     echo -n "> building ${page#pages/}... "
-    sh scripts/buildpage.sh "$page" # ... la construire avec notre script
+    bash scripts/buildpage.sh "$page" # ... la construire avec notre script
     echo "done."
 done
 echo "Finished!"
